d1f81f01ae78e422ce6216ab4146eb62be5b3b28
Santiago <saruizt@unal.edu.co>
2024-02-18 11:31:51 -0500

    Se crearon las clases con sus atributos y métodos. Pendiente de revisión

---
+++ main.py
@@ -0,0 +1,43 @@
+class Asiento:
+    def __init__(self, color, precio, registro):
+        self.color=color
+        self.precio=precio
+        self.registro=registro
+    def cambiarColor(self, color:str):
+        if color=="rojo" or color =="verde" or color=="amarillo" or color=="negro" or color=="blanco":
+            self.color=color
+
+class Auto:
+    cantidadCreados=0
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+        self.modelo=modelo
+        self.precio=precio
+        self.asientos=asientos
+        self.marca=marca
+        self.motor=motor
+        self.registro=registro
+    def cantidadAsientos(self):
+        contador=0
+        for i in self.asientos:
+            contador+=1
+        return contador
+    def verificarIntergridad(self):
+        if self.resgistro==self.motor.registro:
+            for i in self.asientos:
+                if self.asientos[i].registro!=self.registro:
+                    return "Las piezas no son originales"
+            return "Auto original" 
+        else:
+            return "Las piezas no son originales"
+    
+            
+class Motor:
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros=numeroCilindros
+        self.tipo=tipo
+        self.registro=registro
+    def cambiarRegistro(self, int):
+        self.registro=int
+    def asignarTipo(self, String):
+        if String=="electrico" or String== "gasolina":
+            self.tipo=String
